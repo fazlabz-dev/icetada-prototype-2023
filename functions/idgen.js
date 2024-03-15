99ab3d4f42fc0e47b2a693429ac0b52e3f88ba66
TemaisgameNoobas <60001674+TemaisgameNoobas@users.noreply.github.com>
2024-03-16 02:26:37 +0700

    Update idgen.js

--- functions/idgen.js
+++ functions/idgen.js
@@ -1,10 +1,11 @@
 module.exports = function(){
 
-    function genId(){
-        var shortid = require('shortid');
-        shortid.length(10);
-        var idg = shortid.generate();
-        return idg;
-    }
+			function generaterandID(chars, length) {
+				var retVal = "";
+				for (var i = 0, n = chars.length; i < length; ++i) {
+					retVal += chars.charAt(Math.floor(Math.random() * n));
+				}
+				return retVal;
+			}
 
 }
