05657379072cc4a1948b89333602604096ccf278
TemaisgameNoobas <60001674+TemaisgameNoobas@users.noreply.github.com>
2024-03-16 02:44:56 +0700

    Update idgen.js

--- functions/idgen.js
+++ functions/idgen.js
@@ -1,7 +1,8 @@
 module.exports = {
 
-			generaterandID: function (chars, length) {
+			generaterandID: function (length) {
 				var retVal = "";
+				var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
 				for (var i = 0, n = chars.length; i < length; ++i) {
 					retVal += chars.charAt(Math.floor(Math.random() * n));
 				}
